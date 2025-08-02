d6b5ee61ab3b8ef7fcda574d8bc58a1daeec25ea
Louis Lefebvre <louislefebvre1999@gmail.com>
2025-08-01 22:18:19 -0500

    fix: Update version cmd to get updated by mvn
    ```bash
    [louis@louarch1 blouis](main)$ java -jar ./target/blou-0.1.0-SNAPSHOT.jar version
    blou version 0.1.0-SNAPSHOT
    ```

--- src/main/java/com/github/louislef299/blouis/App.java
+++ src/main/java/com/github/louislef299/blouis/App.java
@@ -3,6 +3,9 @@ package com.github.louislef299.blouis;
 import com.github.rvesse.airline.Cli;
 import com.github.rvesse.airline.builder.CliBuilder;
 import com.github.rvesse.airline.annotations.Command;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class App {
     
@@ -10,7 +13,21 @@ public class App {
     public static class VersionCommand implements Runnable {
         @Override
         public void run() {
-            System.out.println("blou version 1.0-SNAPSHOT");
+            String version = getVersion();
+            System.out.println("blou version " + version);
+        }
+        
+        private String getVersion() {
+            try (InputStream input = getClass().getClassLoader().getResourceAsStream("version.properties")) {
+                if (input == null) {
+                    return "unknown";
+                }
+                Properties props = new Properties();
+                props.load(input);
+                return props.getProperty("version", "unknown");
+            } catch (IOException e) {
+                return "unknown";
+            }
         }
     }
     
